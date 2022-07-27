bd07ed891b09a989b6e01b962b89011c0154d88e
UrielSpiridione <urielspiridione135@gmail.com>
2022-07-27 12:57:27 -0300

    First commit

---
+++ src/main/java/com/portfolio/backenduspiri/BackendUspiriApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.backenduspiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUspiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUspiriApplication.class, args);
+	}
+
+}
